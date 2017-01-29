5940328763998aab8eb65d6806a9dedbc856858f
eugeneborodin <eugeneborodin@mail.ru>
2017-01-29 04:31:42 +0300

    init

---
+++ CalcFunc.UnitTests/MemCalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CalcFunc.UnitTests
+{
+    [TestFixture]
+    public class MemCalculatorTests
+    {
+        [Test]
+        public void Sum_ByDefault_ReturnsZero()
+        {
+            MemCalculator calc = MakeCalc();
+
+            int lastSum = calc.Sum();
+
+            Assert.AreEqual(0, lastSum);
+        }
+
+        [Test]
+        public void Add_WhenCalled_ChangesSum()
+        {
+            MemCalculator calc = MakeCalc();
+
+            calc.Add(1);
+            int sum = calc.Sum();
+
+            Assert.AreEqual(1, sum);
+
+        }
+
+        private static MemCalculator MakeCalc()
+        {
+            return new MemCalculator();
+        }
+    }
+}
